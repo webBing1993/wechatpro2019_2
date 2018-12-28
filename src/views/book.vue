97083d3d5b340c0cd80eee64b76d850c41f34f41
zoeZheng <1423736181@qq.com>
2018-12-28 11:13:34 +0800

    软键盘fix2

--- src/views/book.vue
+++ src/views/book.vue
@@ -172,8 +172,6 @@ export default {
               }
           })
       }
-      console.log('broswer:',broswer)
-
       const h = document.body.scrollHeight  // 用onresize事件监控窗口或框架被调整大小，先把一开始的高度记录下来
       window.onresize = function () { // 如果当前窗口小于一开始记录的窗口高度，那就让当前窗口等于一开始窗口的高度
           console.log('h:',h)
